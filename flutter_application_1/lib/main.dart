7cd349541401b1a6c22ee72f1face822e9b185cd
Avishi Sharma <robloxpusheen8@gmail.com>
2026-01-17 18:14:59 -0500

    Learning states, setting up base

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -6,20 +6,44 @@ import "dart:math" as math;
 var bars = ["hi", "bye", "hello", "world"];
 var rand = math.Random();
 
-void main()=>runApp(MyApp);
+void main()=>runApp(const MaterialApp(
+    title: "quotes aww",
+    home: MyWidget()
+  ));
 
 
-class MyWidget extends StatelessWidget{
+class MyWidget extends StatefulWidget{
   const MyWidget({super.key});
   @override
+  State<MyWidget> createState()=>_MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget>{
+  String quote = bars[rand.nextInt(bars.length)];
+
+  void newQuote(){
+    setState((){
+      quote=bars[rand.nextInt(bars.length)];
+    });
+  }
+
+  @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      home:Center(
-        child: Text(
-          bars[rand.nextInt(bars.length)], 
-          textDirection: TextDirection.ltr
-          ),
-      ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              quote,
+              textDirection: TextDirection.ltr
+            ),
+            ElevatedButton(
+              onPressed: newQuote, 
+              child: Text("change")
+            )
+          ],
+        )
+      )
     );
   }
 }
